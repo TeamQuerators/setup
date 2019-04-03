0df0cd5eccebea974620d2250de890990162f4d8
TeamQuerators <48662924+TeamQuerators@users.noreply.github.com>
2019-04-03 14:24:31 +0800

    OnMouseDown

---
+++ setup/Assets/Graphics/bincontrol.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bincontrol : MonoBehaviour {
+
+	public Transform bottomObj;
+	public Transform topObj;
+	public Transform spagObj;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnMouseDown()
+	{
+		if (gameObject.name == "plate bin") 
+		{
+			Instantiate (bottomObj, new Vector2 (0, -.5f), bottomObj.rotation);
+			Instantiate (topObj, new Vector2 (0, -.3f), topObj.rotation);
+
+		}
+	}
+
+}
